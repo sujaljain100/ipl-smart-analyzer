42212096e531c7551c0bce78a20c34842fd65062
Sujal Jain <jainsujal100@gmail.com>
2026-05-16 14:51:12 +0530

    IPL Smart Analyzer - Complete EDA Project

---
+++ ipl.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# Load data
+matches = pd.read_csv('matches.csv')
+deliveries = pd.read_csv('deliveries.csv')
+
+# Basic info dekho
+print("=== MATCHES ===")
+#print(matches.shape)
+#print(matches.head())
+#print(matches.info())
+
+#print("\n=== DELIVERIES ===")
+#print(deliveries.shape)
+#print(deliveries.head())
+#print(deliveries.info())
